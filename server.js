200b84d4388e61d01315129674811b594d342359
Bangstry <tylerwesleyquinn@gmail.com>
2019-10-08 17:07:00 -0400

    get request's working

--- server.js
+++ server.js
@@ -6,4 +6,33 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res) => {
+    db
+    .select('*')
+    .from('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
+server.get("/:id", (req, res) => {
+    const id = req.params.id
+
+    db
+    .select('*')
+    .from('accounts')
+    .where('id', id)
+    .then(accounts => {
+        res.status(200).json(accounts)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
+
+
 module.exports = server;
